6088671ac56afd86d6c43175dec2bbe7d7dcab47
Quang Huy <huyquang0310@gmail.com>
2024-09-11 00:42:09 +0700

    edit AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,23 @@
 <?php
 
-namespace App\Providers;
+   namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+   use Illuminate\Support\ServiceProvider;
+   use Illuminate\Pagination\Paginator;
+   use Illuminate\Support\Facades\Blade;
+   use Illuminate\Support\Facades\URL;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
-}
+   class AppServiceProvider extends ServiceProvider
+   {
+       /**
+        * Register any application services.
+        *
+        * @return void
+        */
+       public function register()
+       {
+           if (env('APP_ENV') !== 'local') {
+               URL::forceScheme('https');
+           }
+       }
+   }
